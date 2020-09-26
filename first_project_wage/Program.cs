02f1d826a8d4056f68c2a511b6d1c18dc8ea8985
venkey <venkeyvenkatesh885@gmail.com>
2020-09-26 12:17:49 +0530

    [venkey] attendance added for employee

--- first_project_wage/Program.cs
+++ first_project_wage/Program.cs
@@ -10,6 +10,19 @@ namespace first_project_wage
 
 
             Console.WriteLine("Hello Employee!!! Welcome");
+
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
